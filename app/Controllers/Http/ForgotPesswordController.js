a638cb26d3cdbef1c799f541f5bd6f9120925a57
Bruna Albino <bruhalbino29@gmail.com>
2020-04-02 00:07:36 -0300

    Forgot and reset password

--- app/Controllers/Http/ForgotPesswordController.js
+++ app/Controllers/Http/ForgotPesswordController.js
@@ -1,6 +1,8 @@
 'use strict'
 const User = use('App/Models/User')
+const moment = require('moment')
 const crypto = require('crypto')
+const Mail = use('Mail')
 
 class ForgotPesswordController {
   async store({ request, response }) {
@@ -12,10 +14,44 @@ class ForgotPesswordController {
       user.token_created_at = new Date()
 
       await user.save()
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, },
+        message => {
+          message
+            .to(user.email)
+            .from('Adonis@Team')
+            .subject('Password recovery')
+        }
+      )
+
     } catch (error) {
       return response.status(error.status).send({ error: { message: 'Email não existe' } })
     }
 
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all();
+
+      const user = await User.findByOrFail('token', token);
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) return response.status(401).send({ error: { message: 'Token expirou' } })
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+
+    } catch (error) {
+      return response.status(error.status).send({ error: { message: 'Algo deu errado', message: error.message } })
+    }
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -6,3 +6,4 @@ Route.get('user', 'UserController.list')
 
 Route.post('login', 'SessionController.store')
 Route.post('passwords', 'ForgotPesswordController.store')
+Route.put('passwords', 'ForgotPesswordController.update')
